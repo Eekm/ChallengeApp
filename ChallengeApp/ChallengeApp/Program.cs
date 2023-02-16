629db738d9f080dc6f0137ba94784e77fc9e9b6c
Emmanuel <emmanuel.slowinski@gmail.com>
2023-02-16 18:10:51 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var płeć = "kobieta";
+var age = 33;
+
+
+if (płeć == "kobieta")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+        if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+
+    else
+        if (age == 30)
+    {
+        Console.WriteLine("Kobieta, lat 30");
+    }
+
+    else
+        if (age > 30)
+    {
+        Console.WriteLine("Kobieta powyżej 30 lat");
+    }
+}
+
+
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni mężczyzna");
+    }
+
+}
